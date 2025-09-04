de45aacb2e98e5b88c57209bcfc70b39bb711b8d
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-09-04 12:29:54 +0200

    index method to include 'statuses' and 'technicians' in the props.

--- resources/js/Pages/Tickets/index.vue
+++ resources/js/Pages/Tickets/index.vue
@@ -3,7 +3,7 @@
         <h2>Lista Ticket</h2>
         <ul>
             <li v-for="ticket in tickets" :key="ticket.id">
-                {{ ticket.id }} - {{ ticket.description }} - {{ getAreaName(ticket.area_id) }} - {{ getUserName(ticket.user_id) }}
+                {{ ticket.id }} - {{ ticket.description }} - {{ getAreaName(ticket.area_id) }} - {{ getUserName(ticket.user_id) }} - {{ getStatusName(ticket.status_id) }} - {{ getTechnicianName(ticket.assigned_to) }}
             </li>
         </ul>
     </div>
@@ -14,16 +14,27 @@ export default {
     props: {
         tickets: Array,
         areas: Array,
-        user: Object
+        userLog: Object,
+        statuses: Array,
+        technicians: Array
     },
     methods: {
         getAreaName(areaId) {
-            const area = this.areas.find(a => a.id === areaId);
+            const area = this.areas.find(area => area.id === areaId);
             return area ? area.name : 'N/A';
         },
         getUserName(userId) {
-            return this.user && this.user.id === userId ? this.user.name : 'N/A';
+            return this.userLog && this.userLog.id === userId ? this.userLog.name : 'N/A';
+        },
+        getStatusName(statusId) {
+            const status = this.statuses.find(status => status.id === statusId);
+            return status ? status.name : 'N/A';
+        },
+        getTechnicianName(technicianId) {
+            const technician = this.technicians.find(tech => tech.id === technicianId);
+            return technician ? technician.name : 'N/A';
         }
+       
     }
 }
 </script>
