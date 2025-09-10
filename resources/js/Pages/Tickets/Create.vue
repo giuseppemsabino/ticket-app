1b8ce6b0e8def318d7a86e2843898126129c4d9c
mattiacoli <mattia.coli@aol.com>
2025-09-10 15:46:49 +0200

    refactor: reorganize form fields in Create.vue for improved readability and maintainability

--- resources/js/Pages/Tickets/Create.vue
+++ resources/js/Pages/Tickets/Create.vue
@@ -36,45 +36,47 @@ function submit() {
 
         <form @submit.prevent="submit">
 
+          <div class="options d-flex gap-3 mb-4">
+            <!-- Progetto -->
+            <div class="col mb-3">
+              <label class="form-label">Progetto</label>
+              <select v-model="form.project_id" class="form-select">
+                <option disabled value="">-- Seleziona Progetto --</option>
+                <option v-for="project in props.projects" :key="project.id" :value="project.id">
+                  {{ project.name }}
+                </option>
+              </select>
+              <div v-if="form.errors.project_id" class="text-danger small">
+                {{ form.errors.project_id }}
+              </div>
+            </div>
+
+            <!-- Area -->
+            <div class="col mb-3">
+              <label class="form-label">Area</label>
+              <select v-model="form.area_id" class="form-select">
+                <option disabled value="">-- Seleziona Area --</option>
+                <option v-for="area in props.areas" :key="area.id" :value="area.id">
+                  {{ area.name }}
+                </option>
+              </select>
+              <div v-if="form.errors.area_id" class="text-danger small">
+                {{ form.errors.area_id }}
+              </div>
+            </div>
+          </div>
+
           <!-- Descrizione -->
           <div class="mb-3">
             <label class="form-label">Descrizione</label>
-            <textarea v-model="form.description" class="form-control"
-              placeholder="Inserisci la descrizione del ticket"></textarea>
+            <textarea v-model="form.description" class="form-control" placeholder="Inserisci la descrizione del ticket"
+              rows="5"></textarea>
             <div v-if="form.errors.description" class="text-danger small">
               {{ form.errors.description }}
             </div>
           </div>
 
-          <!-- Area -->
-          <div class="mb-3">
-            <label class="form-label">Area</label>
-            <select v-model="form.area_id" class="form-select">
-              <option disabled value="">-- Seleziona Area --</option>
-              <option v-for="area in props.areas" :key="area.id" :value="area.id">
-                {{ area.name }}
-              </option>
-            </select>
-            <div v-if="form.errors.area_id" class="text-danger small">
-              {{ form.errors.area_id }}
-            </div>
-          </div>
 
-
-
-          <!-- Progetto -->
-          <div class="mb-3">
-            <label class="form-label">Progetto</label>
-            <select v-model="form.project_id" class="form-select">
-              <option disabled value="">-- Seleziona Progetto --</option>
-              <option v-for="project in props.projects" :key="project.id" :value="project.id">
-                {{ project.name }}
-              </option>
-            </select>
-            <div v-if="form.errors.project_id" class="text-danger small">
-              {{ form.errors.project_id }}
-            </div>
-          </div>
 
           <!-- Pulsante salva -->
           <PrimaryButton :disabled="form.processing" :type="'submit'">
